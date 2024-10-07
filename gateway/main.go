1daab48d7f2e5b89631c7ea43126f3d8ae71f428
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-10-07 17:18:05 +0100

    Refactor gateway package main file

--- gateway/main.go
+++ gateway/main.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
-
-	"github.com/gin-gonic/gin"
-	"github.com/ikwemogena/order-management-gateway/config"
-	pb "github.com/ikwemogena/order-management/common/api"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
-
-var (
-	orderServiceAddr = "localhost:3000"
+	"github.com/ikwemogena/order-management/gateway/config"
+	"github.com/ikwemogena/order-management/gateway/routes"
 )
 
 func main() {
-    fmt.Println("hello morgz")
-
-	conn, err := grpc.NewClient(orderServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	if err != nil {
-		log.Fatalf("Failed to dial server: %v", err)
-	}
-
-	defer conn.Close()
-
-	log.Println("Successfully connected to order service", orderServiceAddr)
-
-	c := pb.NewOrderServiceClient(conn)
-
-	log.Println("Successfully created client", c)
 
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "OMS Gateway!",
-		})
-	})
+	r := routes.SetupRouter()
 	
 	log.Printf("Starting server on %s", cfg.ServerAddress)
 	if err := r.Run(cfg.ServerAddress); err != nil {
