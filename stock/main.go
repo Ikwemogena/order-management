b0643ee829f64444b1234e7000a95776fa1ca880
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-10-07 20:32:11 +0100

    Refactor stock service main file and add stock service handler and route setup

--- stock/main.go
+++ stock/main.go
@@ -8,13 +8,9 @@ import (
 	"github.com/ikwemogena/order-management/stock/config"
 	"github.com/ikwemogena/order-management/stock/db"
 	pb "github.com/ikwemogena/order-management/stock/proto"
+	"github.com/ikwemogena/order-management/stock/service"
 	"google.golang.org/grpc"
 )
-
-type server struct {
-	pb.UnimplementedStockServiceServer
-}
-
 
 func main() {
 	cfg, err := config.Load()
@@ -40,7 +36,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterStockServiceServer(s, &server{})
+	pb.RegisterStockServiceServer(s, &service.Server{DB: dbInstance})
 	fmt.Println("Stock service is running on port", cfg.ServerAddress)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
