4288aa2e140f4ab9489263c0c970017f803b9f7e
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-10-14 00:45:49 +0100

    feat: Add payments database package and initialization

---
+++ payments/db/db.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	*sql.DB
+}
+
+func Init(databaseURL string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	
+	return db, nil
+}
+
+func (db *Database) InitTables() error {
+    _, err := db.Exec(`
+       CREATE TABLE IF NOT EXISTS payments (
+            id UUID PRIMARY KEY,
+            order_id UUID NOT NULL,
+            reference string UNIQUE NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            currency VARCHAR(10) DEFAULT 'NGN',
+            status VARCHAR(50) NOT NULL,
+            payment_provider TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        )
+    `)
+    if err != nil {
+        return fmt.Errorf("failed to create payemnts table: %v", err)
+    }
+    return nil
+}
